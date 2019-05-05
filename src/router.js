219aa27455b60dfa8291d2f55023af7368fc0235
bauhaus <uthink@163.com>
2019-05-05 14:37:22 +0800

    project init

---
+++ src/router.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './pages/home/Home'
+// import City from './pages/city/City'
+// import Detail from './pages/detail/Detail'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/city',
+      name: 'City',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "city" */ './pages/city/City.vue')
+    },
+    {
+      path: '/detail',
+      name: 'Detail',
+      component: () => import(/* webpackChumkName: "detail" */ './pages/detail/Detail.vue')
+    }
+  ]
+})
